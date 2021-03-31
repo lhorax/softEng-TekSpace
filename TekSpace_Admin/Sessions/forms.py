7de3a365e72a60e6416fe9c2848feafea758d65c
lhorax <70205933+lhorax@users.noreply.github.com>
2021-03-31 19:23:11 +0800

    deleted redundant files

--- TekSpace_Admin/Sessions/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Session
-
-class SessionForm(forms.ModelForm):
-
-    class Meta:
-        model = Session
-        fields = ['session_code', 'session_name']
